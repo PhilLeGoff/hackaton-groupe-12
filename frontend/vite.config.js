de4ae9364a209008e84e6dd5d6d4d130b4b1dc04
TheoKraichette <kraichettedev@gmail.com>
2026-03-17 11:52:34 +0100

    Update dockerfiles and config for frontend container

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const PORT = env.APP_PORT || 3000;
 
   return {
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
+    plugins: [react()],
     server: {
       host: "0.0.0.0",
       proxy: {
